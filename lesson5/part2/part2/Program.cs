7029ad74fc32f8c5dc4e7bc0e78de43e92987cfe
Misha <19fishka96@mail.ru>
2021-11-19 15:25:49 +0300

    do lesson5

---
+++ lesson5/part2/part2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+
+
+namespace part2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello!");
+            var time = DateTime.Now;
+            string ct = time.ToString("dd MMMM yyyy hh:mm:ss tt");
+            File.AppendAllLines("startup.txt", new []{ct});
+        }
+    }
+}
