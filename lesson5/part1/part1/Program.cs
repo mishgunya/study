67d7c36c394b4134382adbfe5b1b2957bfed5b69
Misha <19fishka96@mail.ru>
2021-11-19 12:15:16 +0300

    do lesson5 part1

--- lesson5/part1/part1/Program.cs
+++ lesson5/part1/part1/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace part1
+﻿using System;
+
+namespace part1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
+            var input = Console.ReadLine();
+            File.WriteAllText("file.txt" ,input);
         }
+        
     }
 }
