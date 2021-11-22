47cd942d036b4ab55457d89d7188e23fabe4430d
Misha <19fishka96@mail.ru>
2021-11-22 12:11:12 +0300

    do lesson7

--- lesson7/part1/part1/Program.cs
+++ lesson7/part1/part1/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace part1
+﻿using System;
+
+namespace part1
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            double num = 0.0;
+            Console.WriteLine("Введите первое число");
+            double int32_1 = (double) Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double int32_2 = (double) Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите операцию (1-сложение,2-вычитание,3-умножение,4-деление)");
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    num = int32_1 + int32_2;
+                    break;
+                case 2:
+                    num = int32_1 - int32_2;
+                    break;
+                case 3:
+                    num = int32_1 * int32_2;
+                    break;
+                case 4:
+                    num = int32_1 / int32_2;
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели число вне диапазона");
+                    break;
+            }
+            Console.WriteLine(string.Format("Ваш результат:{0}", (object) num));
+            Console.WriteLine("Нажмите любую клавишу для выхода");
+            Console.ReadKey();
+        }
     }
-  }
 }
--- lesson7/part1/part1/Properties/AssemblyInfo.cs
+++ lesson7/part1/part1/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9B401984-5A82-4C4D-9B46-9B0C4E5F7062")]
+[assembly: Guid("1FCCBD3E-BFBC-4636-AB8E-32C9E1103471")]
 
 // Version information for an assembly consists of the following four values:
 //
